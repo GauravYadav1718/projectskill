f4ecdf1669ffea654a779a025c5af64f09098536
GauravYadav1718 <gybawana@gmail.com>
2025-07-28 20:07:16 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3285;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN ,
   credentials: true
 }));
 
