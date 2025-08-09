955baec4da821052cd6080fe0aec51f02c2c6e8e
GauravYadav1718 <gybawana@gmail.com>
2025-08-09 13:59:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+
 const app = express();
+const PORT = process.env.PORT || 3285;
 
 
 // Middleware
