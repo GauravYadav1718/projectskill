437484bd841900c3d531b581566f8c40359fcedf
GauravYadav1718 <gybawana@gmail.com>
2025-08-09 20:48:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,16 +8,16 @@ const app = express();
 const PORT = process.env.PORT || 3285;
 
 
-// Middleware
+
 app.use(cors({
-  origin: 'https://projectskill-8rue-git-main-gauravyadav1718s-projects.vercel.app' ,
+  origin: 'https://projectskill-8rue.vercel.app' ,
   credentials: true
 }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// MongoDB Connection
+
 mongoose.connect( "mongodb+srv://gaurav:gaurav123%40@cluster0.k0kjrm4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
  useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -25,16 +25,16 @@ mongoose.connect( "mongodb+srv://gaurav:gaurav123%40@cluster0.k0kjrm4.mongodb.ne
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log('MongoDB Connection Error:', err));
 
-// Routes
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/skills', require('./routes/skills'));
 app.use('/api/requests', require('./routes/requests'));
 app.use('/api/messages', require('./routes/message'));
 
 // Test route
-app.get('/', (req, res) => {
-  res.json({ message: 'SkillSwap API is running!' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ message: 'SkillSwap API is running!' });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
